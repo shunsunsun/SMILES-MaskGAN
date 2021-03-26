59ebc8e73cb7450e9a1f6f2e4e4f19aae9020c25
dudwojae <46015392+dudwojae@users.noreply.github.com>
2021-03-26 13:29:35 +0900

    Update mypath.py

--- utils/mypath.py
+++ utils/mypath.py
@@ -2,8 +2,4 @@ import os
 
 # For ubuntu
 data_path = '/workspace/FileStorage/Chemistry_final/data'
-save_path = '/workspace/FileStorage/Chemistry_final/results/mask_50_0.3_exp'
-
-# For window
-# data_path = 'C:/Users/youngjae/OneDrive - korea.ac.kr/PycharmProjects/Chemistry_final/data'
-# save_path = '../results/mask_50_0.1_exp_result'
+save_path = '/workspace/FileStorage/Chemistry_final/results/mask_0.1_exp'
